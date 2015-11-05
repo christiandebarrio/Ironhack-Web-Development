a42040c43e52f6633fc3b93d19227c647c48173b
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-05 17:25:42 +0100

    post_details

--- week2/ironblog_sinatra/server.rb
+++ week2/ironblog_sinatra/server.rb
@@ -4,16 +4,24 @@ require 'sinatra/reloader'
 require_relative './lib/blog.rb'
 require_relative './lib/post.rb'
 
-my_blog = Blog.new
+blog = Blog.new
 post1 = Post.new("Título Post1", "Texto del primer post1")
 post2 = Post.new("Título Post2", "Texto del primer post2")
 post3 = Post.new("Título Post3", "Texto del primer post3")
-my_blog.add_post(post1)
-my_blog.add_post(post2)
-my_blog.add_post(post3)
+blog.add_post(post1)
+blog.add_post(post2)
+blog.add_post(post3)
+time_format = "%Y-%m-%d - %H:%M"
 
 get "/" do
-  @my_posts = my_blog.latest_posts
-  @time_format = "%Y-%m-%d ··· %H:%M"
+  @posts = blog.latest_posts
+  @time_format = time_format
   erb(:home)
+end
+
+get "/post_details/:index" do
+  @index = params[:index].to_i
+  @posts = blog.latest_posts
+  @time_format = time_format
+  erb(:post_details)
 end
--- week2/ironblog_sinatra/spec/server_spec.rb
+++ week2/ironblog_sinatra/spec/server_spec.rb
@@ -13,4 +13,9 @@ describe 'Server Service' do
     expect(last_response).to be_ok
   end
 
+  it "should load the post_details page" do
+    get '/post_details/0'
+    expect(last_response).to be_ok
+  end
+
 end
