5ffe9639cc200fe005bf297e285e31c51c58f606
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-11 17:23:04 +0100

    erase console.log

--- week3/javascript/caesarCipher/caesarCipher.js
+++ week3/javascript/caesarCipher/caesarCipher.js
@@ -3,16 +3,17 @@ function caesarCipher (message) {
 
   var code_array = arr_message.map(function (char) {
     if (char >= "A" && char <= "Z" || char >= "a" && char <= "z") {
-      return String.fromCharCode(char.charCodeAt(0)-3);
+      return String.fromCharCode(char.charCodeAt(0) - 3);
     } else {
       return char
     }
   })
-  console.log(code_array)
+
   return code_array.join("");
 }
 
 var encrypted = caesarCipher("brutus");
 encrypted = caesarCipher("Et tu, brute?");
+
 
 console.log(encrypted);
