b3a2c28bff3df39cf94bd90d1184be39ad2b3699
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-11 17:38:24 +0100

    iteration 3. shift_number with default value -3

--- week3/javascript/caeserCipher/caeserCipher.js
+++ week3/javascript/caeserCipher/caeserCipher.js
@@ -1,9 +1,13 @@
 function caeserCipher (message, shiftNumber) {
   var arr_message = message.split("");
 
+  if (shiftNumber === undefined) { 
+    shiftNumber = -3; 
+  }
+
   var code_array = arr_message.map(function (char) {
     if (char >= "A" && char <= "Z" || char >= "a" && char <= "z") {
-      return String.fromCharCode(char.charCodeAt(0) - shiftNumber);
+      return String.fromCharCode(char.charCodeAt(0) + shiftNumber);
     } else {
       return char
     }
@@ -12,8 +16,6 @@ function caeserCipher (message, shiftNumber) {
   return code_array.join("");
 }
 
-var encrypted = caeserCipher("brutus");
-encrypted = caeserCipher("Et tu, brute?");
-encrypted = caeserCipher("Et tu, brute?", -4);
+var encrypted = caeserCipher("Et tu, brute?", -4);
 
 console.log(encrypted);
