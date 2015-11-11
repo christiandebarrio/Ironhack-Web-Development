30ebea3872b6fe5d4afbe1ebcaee039e94e4dadb
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-11-11 23:49:05 +0100

    iteration 4. fixed code letter off

--- week3/javascript/caeserCipher/caeserCipher.js
+++ week3/javascript/caeserCipher/caeserCipher.js
@@ -6,16 +6,40 @@ function caeserCipher (message, shiftNumber) {
   }
 
   var code_array = arr_message.map(function (char) {
-    if (char >= "A" && char <= "Z" || char >= "a" && char <= "z") {
-      return String.fromCharCode(char.charCodeAt(0) + shiftNumber);
+    var charCode = String.fromCharCode(char.charCodeAt(0) + shiftNumber);
+    if (char >= "A" && char <= "Z") {
+      return testUpperCase(charCode);
+    } else if (char >= "a" && char <= "z") {
+      return testLowerCase(charCode);
     } else {
-      return char
+      return char;
     }
+    return code_array
   })
 
   return code_array.join("");
 }
 
-var encrypted = caeserCipher("Et tu, brute?", -4);
+function testUpperCase (char) {
+  if (char < "A") {
+    return String.fromCharCode(char.charCodeAt(0) + 26);
+  } else if (char > "Z") {
+    return String.fromCharCode(char.charCodeAt(0) - 26);
+  } else {
+    return char;
+  }
+}
+
+function testLowerCase (char) {
+  if (char < "a") {
+    return String.fromCharCode(char.charCodeAt(0) + 26);
+  } else if (char > "z") {
+    return String.fromCharCode(char.charCodeAt(0) - 26);
+  } else {
+    return char;
+  }
+}
+
+var encrypted = caeserCipher("Et tu, brute?", 6)
 
 console.log(encrypted);
