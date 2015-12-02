a98ae77dea397b7f201cb9031f9e2ec624fa2c9b
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-12-02 11:47:26 +0100

    notes

--- week6/devise/db/seeds.rb
+++ week6/devise/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Seed some users
+
+clark, tony, rick = User.create([
+
+
+])
